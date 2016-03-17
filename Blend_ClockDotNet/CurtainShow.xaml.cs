d6c5a1fb90b44fa39bbc77f574a2f7ae7cf11063
chengy <cosmicy@outlook.com>
2016-03-17 18:08:17 +0800

    *完成教程5 +显示Curtain

--- Blend_ClockDotNet/CurtainShow.xaml.cs
+++ Blend_ClockDotNet/CurtainShow.xaml.cs
@@ -10,33 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Blend_ClockDotNet
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// Window2.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window2 : Window
     {
-        public MainWindow()
+        public Window2()
         {
             InitializeComponent();
-        }
-
-
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 w = new Window1();
-            w.Show();
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            Window2 w = new Window2();
-            w.Show();
         }
     }
 }
